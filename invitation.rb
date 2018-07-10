7de18c9010027ad1eca6a2acae2ab3bca6efeb13
AminahAliu <aaliu2021@chadwickschool.org>
2018-07-10 16:38:05 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -16,10 +16,14 @@ time = gets.strip
 
 puts "host_name?"
 host_name = gets.strip
+
+puts "rsvp_by?"
 rsvp_by = gets.strip
+
 def invite 
   puts "Dear #{guest_name}"
   puts "You are cordially invited to the #{party_name} on #{date} at #{time}. Please RSVP no later than #{rsvp_by}."
   puts "Sincerely,"
   puts "   #{host_name}"
 end 
+invite 
